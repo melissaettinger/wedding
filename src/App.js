ebc5c80a643a200af96999fa965e32cd8914e87a
Melissa Ettinger <mjettinger85@gmail.com>
2017-09-24 00:54:56 +0800

    Adding material design and hosted images

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Button from 'material-ui/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Tom and Melissa Wedding</h2>
+          <h2>Tom and Melissa</h2>
+          <div>
+            <Button onClick={() => alert('test')}>Click here</Button>
+          </div>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          We are getting married!
         </p>
-        <p> Test </p>
       </div>
     );
   }
